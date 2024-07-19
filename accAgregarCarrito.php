44bb8a515474b030c164699b9f045e572fd18f18
Mailen Monney <91993023+maimonney@users.noreply.github.com>
2024-07-19 20:29:22 -0300

    Add files via upload
    Actualización 9.4

---
+++ accAgregarCarrito.php
@@ -0,0 +1,30 @@
+<?php
+require_once "../../funciones/autoload.php";
+
+try {
+    date_default_timezone_set('America/Argentina/Buenos_Aires');
+    
+    $usuario_id = htmlspecialchars($_SESSION['login']['usuario_id']);
+    $fecha_horario = date('Y-m-d H:i:s');
+    $total = htmlspecialchars($_POST["total"]);
+
+    $carrito = new Carrito();
+    $carrito->insert($usuario_id, $fecha_horario, $total);
+
+    print_r($carrito);
+    $carrito_id = $carrito->getIdCarrito();
+    print_r($carrito_id);
+
+    if ($carrito_id) {
+        $detallesCompra = new DetallesCompra();
+
+        $carrito->insertarDetallesCompra($detallesCompra, $carrito_id);
+
+        header("location: ../../index.php?sec=compraFinalizada");
+        exit();
+    } else {
+        throw new Exception("Error al obtener el ID del carrito.");
+    }
+} catch (Exception $e) {
+    die($e->getMessage());
+}
